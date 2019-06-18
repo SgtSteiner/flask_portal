d5cb5135cdad61968f4f8246bf3116b8b3f5bd22
SgtSteiner <futitotal@gmail.com>
2019-06-18 20:39:15 +0200

    add views & change to bbdd postgresql

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 class Config(object):
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
+    POSTS_PER_PAGE = 10
 
 
 class ProdConfig(Config):
-    pass
+    SECRET_KEY = "9l\xbe(&\xf5\xe8`\x98@P\xb8\xf4\xa4\x807\xc5\x84\xf9\xcf~}\xa0+"
 
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:niblas69$@localhost:5432/flask_db"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SECRET_KEY = "\x91\xef\xf9V\xa6f\xda\xad!\x95N\xd2\xee\x19\xf3\xc2\x82mm\x87\xa2\x00\x97\xdc"
--- migrations/versions/aef35efde787_initial_migration.py
+++ migrations/versions/aef35efde787_initial_migration.py
@@ -1,8 +1,8 @@
 """initial migration
 
-Revision ID: 40e6f3dc2877
+Revision ID: aef35efde787
 Revises: 
-Create Date: 2019-06-15 18:24:39.090577
+Create Date: 2019-06-17 21:06:20.971254
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '40e6f3dc2877'
+revision = 'aef35efde787'
 down_revision = None
 branch_labels = None
 depends_on = None

7ac2e71db3eb1cba614974bbfb983838c378af35
SgtSteiner <futitotal@gmail.com>
2019-06-18 20:51:39 +0200

    corrección de imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm as Form
 from wtforms import StringField, TextAreaField

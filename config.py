377b2082374046dcac52a755007da2ebc2eedcd6
SgtSteiner <futitotal@gmail.com>
2019-06-22 13:33:29 +0200

    factory structure

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SQLALCHEMY_ECHO = True
     POSTS_PER_PAGE = 10
+    SECRET_KEY = "9l\xbe(&\xf5\xe8`\x98@P\xb8\xf4\xa4\x807\xc5\x84\xf9\xcf~}\xa0+"
 
 
 class ProdConfig(Config):
-    SECRET_KEY = "9l\xbe(&\xf5\xe8`\x98@P\xb8\xf4\xa4\x807\xc5\x84\xf9\xcf~}\xa0+"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:niblas69$@localhost:5432/flask_db"
     # SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    SECRET_KEY = "\x91\xef\xf9V\xa6f\xda\xad!\x95N\xd2\xee\x19\xf3\xc2\x82mm\x87\xa2\x00\x97\xdc"
